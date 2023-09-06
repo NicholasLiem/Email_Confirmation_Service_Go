303eab03e53541dde13211f6922448d3335bcbca
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-09-06 15:58:00 +0700

    feat: add gorrila mux

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
-	"github.com/NicholasLiem/Email_Confirmation_Service_Go/database"
+	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
+	r := mux.NewRouter()
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	db := database.SetupDB()
+	r.HandleFunc("/", TestHandler)
+	//db := database.SetupDB()
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:" + os.Getenv("PORT"),
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	fmt.Println("Listening on localhost:" + os.Getenv("PORT"))
+	log.Fatal(srv.ListenAndServe())
+}
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
